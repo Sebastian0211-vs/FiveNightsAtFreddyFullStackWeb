1ea182dd24a0712deedd18356e8bfe0e74f05fb0
Sebastian0211-vs <sebastian.morsch@edu.vs.ch>
2026-03-12 15:16:59 +0100

    added face detection api

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Login    from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
+import Login        from './pages/Login.jsx';
+import Register     from './pages/Register.jsx';
+import FearDetector from './pages/FearDetector.jsx'; // ← ajoute ça
 
 export default function App() {
     return (
         <Routes>
             <Route path="/login"    element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/test"     element={<FearDetector />} /> {/* ← majuscule */}
             <Route path="*"         element={<Navigate to="/login" replace />} />
         </Routes>
     );
